ce8ef0e8ab2e2becef18a03d8f73d180d38ccc66
komazirou <takahumikobayashi12192214@gmail.com>
2025-04-13 23:09:32 +0900

    機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -8,21 +9,40 @@ function App() {
 
   const todoNameRef = useRef();
 
+  // taskの追加
   const handleAddTodo = () => {
-    // taskの追加
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
 
-    console.log(todoNameRef.current.value);
+  //checkboxの更新
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
 
-  }
+  // taskの削除
+  const handleDeleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  // taskの数をカウント
+  const countTask = todos.filter((todo) => !todo.completed).length;
 
   return (
     <>
       <div>
-        <TodoList todos={todos} />
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
         <input type="text" ref={todoNameRef}/>
         <button onClick={handleAddTodo}>タスクを追加</button>
-        <button>タスクの削除</button>
-        <div>残りのタスク数: </div>
+        <button onClick={handleDeleteTodo}>タスクの削除</button>
+        <div>残りのタスク数: {countTask}</div>
       </div>
     </>
   );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-function Todo({ todo }) {
-  return <div>
-    <label>
-        <input type="checkbox" checked={todo.completed} readOnly/>
-    </label>
-    {todo.name}</div>;
+function Todo({ todo, toggleTodo }) {
+  const handleTodoClick = () => {
+    toggleTodo(todo.id);
+  };
+  
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          readOnly
+          onChange={handleTodoClick}
+        />
+      </label>
+      {todo.name}
+    </div>
+  );
 }
 
 export default Todo;
